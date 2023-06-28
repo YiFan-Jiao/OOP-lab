1ee1ec9365f76118030b193e244d07e6bdc60bb8
YiFan <jiao1995cn@gmail.com>
2023-06-27 21:13:03 -0500

    lab4 edited

---
+++ lab4/class/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4 
+{
+    public class Client
+    {
+        private string _number;
+
+        private long _creditCard;
+
+        private List<Reservation> _reservations = new List<Reservation>();
+        public void AddReservation(Reservation reservation)
+        {
+            _reservations.Add(reservation);
+        }
+        public Client(string number,long creditCard) 
+        {
+            _number = number;
+            _creditCard = creditCard;
+        }
+    }
+}
---
+++ lab4/class/Hotel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public static class Hotel
+    {
+        private static string _name;
+
+        private static string _address;
+
+        private static List<Room> _rooms = new List<Room>();
+
+        private static List<Client> _clients = new List<Client>();
+
+        private static List<Reservation> _reservations = new List<Reservation>();
+        public static void creatAndAddRoom(string number, int capactiy, Boolean occupied)
+        {
+            Room room = new Room(number, capactiy, occupied);
+            _rooms.Add(room);
+        }
+        public static void creatAndAddClient(string number, long creditCard) 
+        { 
+            Client client = new Client(number, creditCard);
+            _clients.Add(client);
+        }
+        public static void AddReservation(DateTime date, int occupants, bool isCurrent, Client client, Room room) 
+        {
+            Reservation reservation = new Reservation(date, occupants, isCurrent, client, room);
+
+            room.AddReservation(reservation);
+            client.AddReservation(reservation);
+            _reservations.Add(reservation);
+        }
+
+    }
+}
